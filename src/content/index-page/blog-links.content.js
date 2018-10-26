7ad508105b89e69fa80f6af7d542b9e6aa308f45
Miha Jamsek <miha_jamsek@windowslive.com>
2018-10-26 15:29:31 +0200

    added warning for maintainer

--- src/content/index-page/blog-links.content.js
+++ src/content/index-page/blog-links.content.js
@@ -1,5 +1,6 @@
 import {BlogLink} from "./schema/blog-link.class";
 
+// If there are less or more than three BlogLinks it can break design! Keep it at exactly three BlogLinks.
 export const blogLinks = [
     new BlogLink("KumuluzEE 3.0.0 is released!", "https://blog.kumuluz.com/product/announcement/developers/2018/10/03/kumuluzee-3.0.0-release", "product,announcement,developers"),
     new BlogLink("KumuluzEE Testing 1.1.0 released", "https://blog.kumuluz.com/product/developers/2018/10/12/kumuluzee-testing-1.1.0-release", "product,announcement,developers"),
