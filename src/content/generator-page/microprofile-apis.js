c3f769ff3567b0ff06297dc2b097017a8a543ac8
Urban Malc <urban.malc@cloud.si>
2018-12-28 13:34:59 +0100

    Upped Fault Tolerance version

--- src/content/generator-page/microprofile-apis.js
+++ src/content/generator-page/microprofile-apis.js
@@ -8,7 +8,8 @@ export const microprofileAPIsList = [
         new VersionedLabel("2.4.0", "MicroProfile Health Check 1.0", "n/a")
     ], []),
     new CheckboxModel("mpa-faulttolerance", [
-        new VersionedLabel("2.4.0", "MicroProfile Fault Tolerance 1.0", "n/a")
+        new VersionedLabel("2.4.0", "MicroProfile Fault Tolerance 1.0", "n/a"),
+        new VersionedLabel("3.0.0", "MicroProfile Fault Tolerance 1.1", "n/a")
     ], []),
     new CheckboxModel("mpa-metrics", [
         new VersionedLabel("2.4.0", "MicroProfile Metrics 1.1", "n/a")
