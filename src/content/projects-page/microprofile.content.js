a8b58ab27bc84cf907ce7f341a4ed2d373f2e32a
Urban Malc <urban.malc@cloud.si>
2019-01-24 15:15:38 +0100

    Upped MP Rest Client version to 1.1

--- src/content/projects-page/microprofile.content.js
+++ src/content/projects-page/microprofile.content.js
@@ -10,6 +10,6 @@ export const microprofileList = [
     new Project("MicroProfile Fault Tolerance 1.1", "For fault tolerant microservices.", "https://github.com/kumuluz/kumuluzee-fault-tolerance/blob/master/README.md", "https://github.com/kumuluz/kumuluzee-samples/tree/master/kumuluzee-fault-tolerance-hystrix"),
     new Project("MicroProfile Metrics 1.1", "For collection and reporting of performance metrics.", "https://github.com/kumuluz/kumuluzee-metrics/blob/master/README.md", "https://github.com/kumuluz/kumuluzee-samples/tree/master/kumuluzee-metrics"),
     new Project("MicroProfile JWT Authentication 1.0", "For role based access control using OpenID Connect and JSON Web Tokens.", "https://github.com/kumuluz/kumuluzee-jwt-auth/blob/master/README.md", "#"),
-    new Project("MicroProfile REST Client 1.0", "Type-safe approach to invoke RESTful services over HTTP.", "https://github.com/kumuluz/kumuluzee-rest-client/blob/master/README.md", "https://github.com/kumuluz/kumuluzee-samples/tree/master/kumuluzee-rest-client"),
+    new Project("MicroProfile REST Client 1.1", "Type-safe approach to invoke RESTful services over HTTP.", "https://github.com/kumuluz/kumuluzee-rest-client/blob/master/README.md", "https://github.com/kumuluz/kumuluzee-samples/tree/master/kumuluzee-rest-client"),
     new Project("MicroProfile Open Tracing 1.0", "For accessing an OpenTracing compliant Tracer object within your JAX-RS application.", "#", "#")
 ];
