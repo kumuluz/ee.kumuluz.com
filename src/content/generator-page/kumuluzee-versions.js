c76a37848ea32a4ae5d485d2a1014c60f77d09df
Urban Malc <urban.malc@cloud.si>
2019-01-28 13:38:20 +0100

    Upped KumuluzEE version to 3.1.0

--- src/content/generator-page/kumuluzee-versions.js
+++ src/content/generator-page/kumuluzee-versions.js
@@ -1,8 +1,9 @@
 import {KumuluzeeVersion} from "./schema/kumuluzee-version.class";
 
 export const kumuluzEEVersionsList = [
-    new KumuluzeeVersion("3.1.0-SNAPSHOT"),
-    new KumuluzeeVersion("3.0.0", true),
+    new KumuluzeeVersion("3.2.0-SNAPSHOT"),
+    new KumuluzeeVersion("3.1.0", true),
+    new KumuluzeeVersion("3.0.0"),
     new KumuluzeeVersion("2.5.3"),
     new KumuluzeeVersion("2.5.2"),
     new KumuluzeeVersion("2.4.1"),
