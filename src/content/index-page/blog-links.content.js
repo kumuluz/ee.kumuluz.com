1ae06b7042d3faf47f3bd22290fb9d1242ad3e3f
Matjaz B. Juric <matjaz.juric@cloud.si>
2019-03-10 22:33:24 +0100

    Update blog-links.content.js

--- src/content/index-page/blog-links.content.js
+++ src/content/index-page/blog-links.content.js
@@ -2,7 +2,7 @@ import {BlogLink} from "./schema/blog-link.class";
 
 // If there are less or more than three BlogLinks it can break design! Keep it at exactly three BlogLinks.
 export const blogLinks = [
-    new BlogLink("Announcing KumuluzEE 3.1.0!", "https://blog.kumuluz.com/product/announcement/developers/2019/01/28/kumuluzee-3.1.0-release", "product,announcement,developers"),
-    new BlogLink("KumuluzEE JWT Auth 1.1.1 released", "https://blog.kumuluz.com/product/announcement/developers/2019/01/29/kumuluzee-jwt-auth-1.1.1-release", "product,announcement,developers"),
-    new BlogLink("KumuluzEE Health 1.0.2 released", "https://blog.kumuluz.com/product/developers/2019/01/31/kumuluzee-health-release-1.0.2", ["product", "developers"])
+    new BlogLink("KumuluzEE 3.2.0 brings MicroProfile 2.1 compliancy", "https://blog.kumuluz.com/product/announcement/developers/2019/02/28/kumuluzee-3.2.0-release", "product,announcement,developers"),
+    new BlogLink("KumuluzEE JWT Auth 1.1.1 released", "https://blog.kumuluz.com/announcement/community/news/product/2019/03/10/kumuluz-5-years", "announcement,community,news,product"),
+    new BlogLink("Tracing KumuluzEE microservices with Jaeger", "https://blog.kumuluz.com/developers/community/2019/02/11/kumuluzee-opentracing-jaeger", "developers,community")
 ];
