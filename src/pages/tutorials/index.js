a1ed5300347dd29e867dde8b7965d7c407fa879a
Jan Meznarič <jmezna@gmail.com>
2019-03-11 10:05:04 +0100

    minor edit on tutorials page

--- src/pages/tutorials/index.js
+++ src/pages/tutorials/index.js
@@ -63,7 +63,7 @@ const tutorialItems = [
         external: true
     },
     {
-        title: "KumuluzEE Microservices in Go and Java",
+        title: "KumuluzEE microservices in Go and Java",
         content: "In this tutorial we develop two microservices, one in Java and one in Go. Developed microservices use the KumuluzEE Config and Discovery extensions to simplify the configuration management and the service discovery in applications composed from microservices built with different technologies.",
         url: "https://github.com/kumuluz/kumuluzee-go-samples/tree/master/tutorial-microservice-config-discovery",
         external: true
